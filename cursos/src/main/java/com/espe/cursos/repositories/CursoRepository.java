cc143ef2b8c6da65400a0e30966d173c6a1ec9fd
Angel Castillo <agcastillo2@espe.edu.ec>
2025-01-12 19:54:42 -0500

    eliminar

--- cursos/src/main/java/com/espe/cursos/repositories/CursoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.espe.cursos.repositories;
-
-import com.espe.cursos.models.entities.Curso;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CursoRepository extends CrudRepository<Curso, Long>{
-
-}
