d55f97d21eeefc93bbe27bb4fc7769320ef4d3de
Angel Castillo <agcastillo2@espe.edu.ec>
2025-01-12 19:48:58 -0500

    Tarea

---
+++ cursos/src/main/java/com/espe/cursos/repositories/EstudianteRepository.java
@@ -0,0 +1,8 @@
+package com.espe.cursos.repositories;
+
+import com.espe.cursos.models.entities.Estudiante;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EstudianteRepository extends CrudRepository<Estudiante, Long>{
+
+}
